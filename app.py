de15ef685e995647355255d6c757d91d3e5c738f
kyrillosShark <157929603+kyrillosShark@users.noreply.github.com>
2024-10-21 17:58:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -104,10 +104,11 @@ SESSION = create_session()
 
 # Initialize Limiter
 limiter = Limiter(
-    app,
     key_func=get_remote_address,
     default_limits=["100 per day", "10 per hour"]
 )
+
+limiter.init_app(app)
 
 # ----------------------------
 # Database Models
